0cd4f1ba2ae3c53dae30f167176fab67891539af
Utku <balikcilar.utku@gmail.com>
2025-09-12 09:52:45 +0300

    working

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,54 @@
 import { useI18n } from '../i18n';
 import styles from './Home.module.css';
+import { Link } from 'react-scroll';
+import{ FaGithub, FaLinkedin } from 'react-icons/fa';
+import { SiPython, SiJavascript, SiReact } from 'react-icons/si';
 
 export default function Home() {
   const { t } = useI18n();
   return (
     <div className={styles.pageContainer}>
-      <h1 className={styles.title}>{t('home.title')}</h1>
-      <p className={styles.description}>
-        {t('home.desc')}
-      </p>
+            <h1 className={styles.title}>{t('home.title')}</h1>
+            <p className={styles.subtitle}>{t('home.subtitle')}</p>
+      <div className={styles.ctaButtons}>
+        <Link 
+          to="projects" 
+          spy={true} 
+          smooth={true} 
+          offset={-70} 
+          duration={500} 
+          className={`${styles.button} ${styles.ctaPrimary}`}
+        >
+          Projelerimi Gör
+        </Link>
+        <Link 
+          to="bio" 
+          spy={true} 
+          smooth={true} 
+          offset={-70} 
+          duration={500} 
+          className={`${styles.button} ${styles.ctaSecondary}`}
+        >
+          İletişime Geç
+        </Link>
+      </div>
+      <div className={styles.socialLinks}>
+        <a href="https://github.com/balikcilarUtku" target="_blank" rel="noreferrer" className={styles.socialIcon}>
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/utku-balıkçılar-5a8289297/" target="_blank" rel="noreferrer" className={styles.socialIcon}>
+          <FaLinkedin />
+        </a>
+      </div>
+      <div className={styles.techStack}>
+        <p>Teknoloji Bilgim</p>
+        <div className={styles.techIcons}>
+          <SiJavascript />
+          <SiReact />
+          <SiPython />
+        </div>
+      </div>
+
     </div>
   );
 }
