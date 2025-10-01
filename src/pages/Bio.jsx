b7bf5c15310419677c7e70168637f4084e69f78b
Utku <balikcilar.utku@gmail.com>
2025-10-01 10:37:23 +0300

    buttons

--- src/pages/Bio.jsx
+++ src/pages/Bio.jsx
@@ -1,4 +1,5 @@
 import { useI18n } from '../i18n';
+import { Link } from 'react-scroll';
 import styles from './Bio.module.css';
 
 export default function Bio() {
@@ -7,7 +8,17 @@ export default function Bio() {
     <div className={styles.pageContainer}>
       <h1 className={styles.title}>{t('bio.title')}</h1>
               <Link 
-                to="projects" 
+                to="iletisim"
+                spy={true} 
+                smooth={true} 
+                offset={-70} 
+                duration={500} 
+                className={`${styles.button} ${styles.ctaSecondary}`}
+              >
+                {t('bio.iletisim')}
+              </Link>
+              <Link 
+                to="cv"
                 spy={true} 
                 smooth={true} 
                 offset={-70} 
